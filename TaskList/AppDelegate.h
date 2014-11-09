bd9a44ef170ba7674c4af27c74cfb2e0119b36ec
杨长湖 <1227503754@qq.com>
2014-11-09 20:31:28 +0800

    Initial Commit

---
+++ TaskList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaskList
+//
+//  Created by 杨长湖 on 14/11/9.
+//  Copyright (c) 2014年 杨长湖. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
